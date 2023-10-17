02ac3563193ff9d9fdd3098b6de937e02cfc5146
Dawid <dawidn392@gmail.com>
2023-10-17 14:24:34 +0200

    Initial Commit

---
+++ Lab3_memo/Lab3_memoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab3_memoApp.swift
+//  Lab3_memo
+//
+//  Created by Dawid Nalepa on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Lab3_memoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
